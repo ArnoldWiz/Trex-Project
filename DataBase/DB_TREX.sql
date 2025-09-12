fdbbef734de5a10321656da58c099f72af487369
Arnold Reyes <166636441+ArnoldWiz@users.noreply.github.com>
2025-09-12 15:33:39 -0600

    Upgrade DB tabla comentarios

--- DataBase/DB_TREX.sql
+++ DataBase/DB_TREX.sql
@@ -1,79 +1,184 @@
-CREATE SCHEMA IF NOT EXISTS `trex` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `trex` ;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Table `trex`.`empleado`
+-- Schema TREX
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trex`.`empleado` (
-  `idEMPLEADO` INT NOT NULL,
-  `NOMBRE` VARCHAR(45) NOT NULL,
-  `APELLIDO` VARCHAR(45) NOT NULL,
-  `ESTATUS` TINYINT NOT NULL,
-  PRIMARY KEY (`idEMPLEADO`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
 
 -- -----------------------------------------------------
--- Table `trex`.`maquina`
+-- Schema TREX
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trex`.`maquina` (
-  `idMAQUINA` INT NOT NULL,
-  `AREA` VARCHAR(45) NOT NULL,
-  `NUMERO` INT NOT NULL,
-  PRIMARY KEY (`idMAQUINA`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+CREATE SCHEMA IF NOT EXISTS `TREX` DEFAULT CHARACTER SET utf8 ;
+USE `TREX` ;
 
 -- -----------------------------------------------------
--- Table `trex`.`orden`
+-- Table `TREX`.`Empleado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trex`.`orden` (
-  `idORDEN` INT NOT NULL,
-  `FOLIO` VARCHAR(45) NOT NULL,
-  `TALLA` INT NOT NULL,
-  `CANTIDAD` INT NOT NULL,
-  `COLOR` VARCHAR(45) NOT NULL,
-  `MODELO` VARCHAR(45) NOT NULL,
-  `TOTAL_LOTES` INT NOT NULL,
-  `LOTE_TERMI` INT NOT NULL,
-  `FECHA_INICIO` DATETIME NOT NULL,
-  `FECHA_FIN` DATETIME NOT NULL,
-  PRIMARY KEY (`idORDEN`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `TREX`.`Empleado` (
+  `idEmpleado` INT NOT NULL,
+  `Area` VARCHAR(45) NOT NULL,
+  `Nombre` VARCHAR(45) NOT NULL,
+  `Apellidos` VARCHAR(45) NOT NULL,
+  `Estatus` TINYINT NOT NULL,
+  PRIMARY KEY (`idEmpleado`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `trex`.`lote`
+-- Table `TREX`.`Maquina`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trex`.`lote` (
-  `idLOTE` INT NOT NULL,
-  `idORDEN` INT NOT NULL,
-  `idEMPLEADO` INT NOT NULL,
-  `idMAQUINA` INT NOT NULL,
-  `CANTIDAD` INT NOT NULL,
-  `FECHA_REALIZADO` DATETIME NOT NULL,
-  PRIMARY KEY (`idLOTE`),
-  INDEX `Orden_idx` (`idORDEN` ASC) VISIBLE,
-  INDEX `Foreign_Empleado_idx` (`idEMPLEADO` ASC) VISIBLE,
-  INDEX `Foreign_Maquina_idx` (`idMAQUINA` ASC) VISIBLE,
-  CONSTRAINT `Foreign_Empleado`
-    FOREIGN KEY (`idEMPLEADO`)
-    REFERENCES `trex`.`empleado` (`idEMPLEADO`),
-  CONSTRAINT `Foreign_Maquina`
-    FOREIGN KEY (`idMAQUINA`)
-    REFERENCES `trex`.`maquina` (`idMAQUINA`),
+CREATE TABLE IF NOT EXISTS `TREX`.`Maquina` (
+  `idMaquina` INT NOT NULL,
+  `Area` VARCHAR(45) NOT NULL,
+  `Numero` INT NOT NULL,
+  PRIMARY KEY (`idMaquina`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `TREX`.`Modelo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `TREX`.`Modelo` (
+  `idModelo` INT NOT NULL,
+  `Folio` VARCHAR(45) NOT NULL,
+  `Modelo` VARCHAR(45) NOT NULL,
+  `CantidadHilo` INT NOT NULL,
+  PRIMARY KEY (`idModelo`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `TREX`.`OrdenDePedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `TREX`.`OrdenDePedido` (
+  `idOrdenDePedido` INT NOT NULL,
+  `FechaInicio` DATETIME NOT NULL,
+  `FechaFin` DATETIME NULL,
+  PRIMARY KEY (`idOrdenDePedido`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `TREX`.`Pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `TREX`.`Pedido` (
+  `idPedido` INT NOT NULL,
+  `idOrdenPedido` INT NOT NULL,
+  `idModelo` INT NOT NULL,
+  `Talla` INT NOT NULL,
+  `Cantidad` INT NOT NULL,
+  `Color` VARCHAR(45) NOT NULL,
+  `TotalLotes` INT NOT NULL,
+  `LoteTerminado` INT NOT NULL,
+  `FechaInicio` DATETIME NOT NULL,
+  `FechaFin` DATETIME NOT NULL,
+  `FechaPrevista` DATETIME NOT NULL,
+  PRIMARY KEY (`idPedido`),
+  INDEX `Foreign_Modelo_idx` (`idModelo` ASC) VISIBLE,
+  INDEX `Foreign_Orden_idx` (`idOrdenPedido` ASC) VISIBLE,
+  CONSTRAINT `Foreign_Modelo`
+    FOREIGN KEY (`idModelo`)
+    REFERENCES `TREX`.`Modelo` (`idModelo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `Foreign_Orden`
-    FOREIGN KEY (`idORDEN`)
-    REFERENCES `trex`.`orden` (`idORDEN`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    FOREIGN KEY (`idOrdenPedido`)
+    REFERENCES `TREX`.`OrdenDePedido` (`idOrdenDePedido`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `TREX`.`Lote`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `TREX`.`Lote` (
+  `idLote` INT NOT NULL,
+  `idOrden` INT NOT NULL,
+  `idEmpTejido` INT NULL,
+  `idEmpPlancha` INT NULL,
+  `idEmpCorte` INT NULL,
+  `idMquTejido` INT NULL,
+  `idMaqPlancha` INT NULL,
+  `idMaqCorte` INT NULL,
+  `Cantidad` INT NOT NULL,
+  `FechaTermTejido` DATETIME NULL,
+  `FechaTermPlancha` DATETIME NULL,
+  `FechaTermCorte` DATETIME NULL,
+  `FechaEmpa` DATETIME NULL,
+  PRIMARY KEY (`idLote`),
+  INDEX `Orden_idx` (`idOrden` ASC) VISIBLE,
+  INDEX `Foreign_Empleado_idx` (`idEmpTejido` ASC) VISIBLE,
+  INDEX `Foreign_Maquina_idx` (`idMquTejido` ASC) VISIBLE,
+  INDEX `Foreign_EmpPlancha_idx` (`idEmpPlancha` ASC) VISIBLE,
+  INDEX `Foreign_EmpCorte_idx` (`idEmpCorte` ASC) VISIBLE,
+  INDEX `Foreign_MaqPlancha_idx` (`idMaqPlancha` ASC) VISIBLE,
+  INDEX `Foreign_MaqCorte_idx` (`idMaqCorte` ASC) VISIBLE,
+  CONSTRAINT `Foreign_Orden`
+    FOREIGN KEY (`idOrden`)
+    REFERENCES `TREX`.`Pedido` (`idPedido`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_EmpTejido`
+    FOREIGN KEY (`idEmpTejido`)
+    REFERENCES `TREX`.`Empleado` (`idEmpleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_MaqTejido`
+    FOREIGN KEY (`idMquTejido`)
+    REFERENCES `TREX`.`Maquina` (`idMaquina`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_EmpPlancha`
+    FOREIGN KEY (`idEmpPlancha`)
+    REFERENCES `TREX`.`Empleado` (`idEmpleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_EmpCorte`
+    FOREIGN KEY (`idEmpCorte`)
+    REFERENCES `TREX`.`Empleado` (`idEmpleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_MaqPlancha`
+    FOREIGN KEY (`idMaqPlancha`)
+    REFERENCES `TREX`.`Maquina` (`idMaquina`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_MaqCorte`
+    FOREIGN KEY (`idMaqCorte`)
+    REFERENCES `TREX`.`Maquina` (`idMaquina`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `TREX`.`ComentariosMaquinas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `TREX`.`ComentariosMaquinas` (
+  `idComentariosMaquinas` INT NOT NULL,
+  `idMaquina` INT NOT NULL,
+  `idEmpleado` INT NOT NULL,
+  `Comentario` TEXT NOT NULL,
+  `FechaRegistro` DATETIME NOT NULL,
+  `Solucionado` TINYINT NOT NULL,
+  PRIMARY KEY (`idComentariosMaquinas`),
+  INDEX `Foreign_Maquina_idx` (`idMaquina` ASC) VISIBLE,
+  INDEX `Foreign_Empleado_idx` (`idEmpleado` ASC) VISIBLE,
+  CONSTRAINT `Foreign_Maquina`
+    FOREIGN KEY (`idMaquina`)
+    REFERENCES `TREX`.`Maquina` (`idMaquina`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `Foreign_Empleado`
+    FOREIGN KEY (`idEmpleado`)
+    REFERENCES `TREX`.`Empleado` (`idEmpleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
