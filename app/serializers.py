8dc48a9ea86780bb870ca63fdd35891a9fb98125
chels <s22120160@alumnos.itsur.edu.mx>
2025-11-01 09:03:45 -0600

    En lugar de utilizar campos unicamente se atrae todo el objeto de la entidad en los Serializers, cambio de variables después de consultar la API.

--- app/serializers.py
+++ app/serializers.py
@@ -4,66 +4,125 @@ from .models import (
     Pedido, Ordendepedido, Comentariosmaquinas
 )
 
-
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['idcliente', 'nombre', 'contacto', 'estatus']
+        fields = '__all__'
 
 
 class ModeloSerializer(serializers.ModelSerializer):
     class Meta:
         model = Modelo
-        fields = ['idmodelo', 'folio', 'modelo', 'cantidadhilo', 'estatus']
-
-
-class LoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lote
-        fields = [
-            'idlote', 'idorden', 'idemptejido', 'idempplancha', 'idempcorte',
-            'idmqutejido', 'idmaqplancha', 'idmaqcorte', 'cantidad',
-            'fechatermtejido', 'fechatermplancha', 'fechatermcorte', 'fechaempa'
-        ]
+        fields = '__all__'
 
 
 class EmpleadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empleado
-        fields = ['idempleado', 'area', 'nombre', 'apellidos', 'estatus']
+        fields = '__all__'
 
 
 class MaquinaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Maquina
-        fields = ['idmaquina', 'area', 'numero', 'estatus']
+        fields = '__all__'
 
 
 class OrdendepedidoSerializer(serializers.ModelSerializer):
-    cliente_nombre = serializers.CharField(source='idcliente.nombre', read_only=True)
+    cliente = ClienteSerializer(read_only=True)
+    cliente_id = serializers.PrimaryKeyRelatedField(
+        source='idcliente',
+        queryset=Cliente.objects.all(),
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Ordendepedido
-        fields = ['idordendepedido', 'numeroorden', 'idcliente', 'cliente_nombre', 'fechainicio', 'fechafin']
+        fields = [
+            'idordendepedido', 'numeroorden', 'cliente', 'cliente_id',
+            'fechainicio', 'fechafin'
+        ]
 
 
 class PedidoSerializer(serializers.ModelSerializer):
-    modelo_folio = serializers.CharField(source='idmodelo.folio', read_only=True)
-    orden_numero = serializers.CharField(source='idordenpedido.numeroorden', read_only=True)
+    modelo = ModeloSerializer(source='idmodelo', read_only=True)
+    orden = OrdendepedidoSerializer(source='idordenpedido', read_only=True)
+
+    modelo_id = serializers.PrimaryKeyRelatedField(
+        source='idmodelo',
+        queryset=Modelo.objects.all(),
+        write_only=True,
+        required=False
+    )
+    orden_id = serializers.PrimaryKeyRelatedField(
+        source='idordenpedido',
+        queryset=Ordendepedido.objects.all(),
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Pedido
         fields = [
-            'idpedido', 'idordenpedido', 'idmodelo', 'modelo_folio', 'talla',
-            'cantidad', 'color', 'totallotes', 'loteterminado', 'fechainicio',
-            'fechafin', 'fechaprevista', 'orden_numero'
+            'idpedido',
+            'orden', 'orden_id',
+            'modelo', 'modelo_id',
+            'talla', 'cantidad', 'color',
+            'totallotes', 'loteterminado',
+            'fechainicio', 'fechafin', 'fechaprevista'
         ]
 
 
+class LoteSerializer(serializers.ModelSerializer):
+    emp_tejido = EmpleadoSerializer(source='idemptejido', read_only=True)
+    emp_plancha = EmpleadoSerializer(source='idempplancha', read_only=True)
+    emp_corte = EmpleadoSerializer(source='idempcorte', read_only=True)
+
+    maq_tejido = MaquinaSerializer(source='idmqutejido', read_only=True)
+    maq_plancha = MaquinaSerializer(source='idmaqplancha', read_only=True)
+    maq_corte = MaquinaSerializer(source='idmaqcorte', read_only=True)
+
+    emp_tejido_id = serializers.PrimaryKeyRelatedField(
+        source='idemptejido', queryset=Empleado.objects.all(), write_only=True, required=False
+    )
+    emp_plancha_id = serializers.PrimaryKeyRelatedField(
+        source='idempplancha', queryset=Empleado.objects.all(), write_only=True, required=False
+    )
+    emp_corte_id = serializers.PrimaryKeyRelatedField(
+        source='idempcorte', queryset=Empleado.objects.all(), write_only=True, required=False
+    )
+
+    maq_tejido_id = serializers.PrimaryKeyRelatedField(
+        source='idmqutejido', queryset=Maquina.objects.all(), write_only=True, required=False
+    )
+    maq_plancha_id = serializers.PrimaryKeyRelatedField(
+        source='idmaqplancha', queryset=Maquina.objects.all(), write_only=True, required=False
+    )
+    maq_corte_id = serializers.PrimaryKeyRelatedField(
+        source='idmaqcorte', queryset=Maquina.objects.all(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Lote
+        fields = '__all__'
+
+
+
 class ComentariosMaquinasSerializer(serializers.ModelSerializer):
-    empleado_nombre = serializers.CharField(source='idempleado.nombre', read_only=True)
-    maquina_numero = serializers.IntegerField(source='idmaquina.numero', read_only=True)
+    empleado = EmpleadoSerializer(source='idempleado', read_only=True)
+    maquina = MaquinaSerializer(source='idmaquina', read_only=True)
+    
+    empleado_id = serializers.PrimaryKeyRelatedField(
+        source='idempleado', queryset=Empleado.objects.all(), write_only=True, required=False
+    )
+    maquina_id = serializers.PrimaryKeyRelatedField(
+        source='idmaquina', queryset=Maquina.objects.all(), write_only=True, required=False
+    )
 
     class Meta:
         model = Comentariosmaquinas
-        fields = ['idcomentariosmaquinas', 'idmaquina', 'maquina_numero', 'idempleado', 'empleado_nombre', 'comentario', 'fecharegistro', 'solucionado']
+        fields = [
+            'idcomentariosmaquinas', 'comentario', 'fecharegistro', 'solucionado',
+            'empleado', 'empleado_id', 'maquina', 'maquina_id'
+        ]
