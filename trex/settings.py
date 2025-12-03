c60cec48a8b89cf94a906148b8b2293cc4509219
Bismk11 <s22120167@alumnos.itsur.edu.mx>
2025-12-03 12:54:16 -0600

    Modificacion de BaseDatos

--- trex/settings.py
+++ trex/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'trex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'app' / 'templates'], # <--- RECOMENDACIÓN: Asegura que busque aquí
+        'DIRS': [BASE_DIR / 'app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
